2d30204a03f7a20a66f662f5f13b77617a32bc0f
Mandi Awale <mawale@ap.equinix.com>
2020-09-13 14:59:23 +1000

    Fixed menubar and content window grids

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,25 +1,61 @@
 import { Grid, Container, Typography } from "@material-ui/core";
-
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(1),
+      textAlign: 'center',
+      color: theme.palette.text.secondary
+    },
+}));
+
+
+
 const Body = () => {
+
+const classes = useStyles();
+
     return (  
-        <Grid container spacing={3}>
-            <Paper elevation={0}>
-                <Grid container item >
-                    <Typography> left</Typography>
-                    <Typography> left</Typography>
-                    <Typography> left</Typography>
-                </Grid>
-                <Grid container item>
-                    <Typography> Right</Typography>
-                    <Typography> Right</Typography>
-                    <Typography> Right</Typography>
-                    <Typography> Right</Typography>
-                </Grid>
-            </Paper>
-        </Grid>
+        <div className={classes.root}>
+            <Grid container spacing={1} justify="space-evenly" alignItems="stretch">
+                    <Grid container item xs={4}>
+                        <Paper className="paper">
+                          <Typography variant="body2">
+                          Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar
+                           Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar
+
+                           Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar
+
+                           Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar
+
+                           Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar Menu Bar
+
+                          </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid container item xs={8}>
+                        <Paper className="paper">
+                           <Typography variant="body2">
+                                Content Window Content Window Content Window Content Window Content Window Content Window Content Window Content Window
+                                Content Window Content Window Content Window Content Window Content Window Content Window Content Window Content Window
+
+                                Content Window Content Window Content Window Content Window Content Window Content Window Content Window Content Window
+
+                                Content Window Content Window Content Window Content Window Content Window Content Window Content Window Content Window
+
+                                Content Window Content Window Content Window Content Window Content Window Content Window Content Window Content Window
+                                Content Window Content Window Content Window Content Window Content Window Content Window Content Window Content Window
+
+                           </Typography>
+                        </Paper>
+                    </Grid>
+            </Grid>
+        </div>
     );
 }
  
