64610a0228701d22eaea472cfac376b4b5c23ab9
awalemandi <mandi.awale@gmail.com>
2020-09-17 08:52:20 +1000

    pre merge

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { BottomNavigationAction, Typography, Container, IconButton, Button, BottomNavigation } from '@material-ui/core';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 
 const Footer = () => {
     return (  
           <div>
-            <BottomNavigation>
-                <Typography variant="body1">Footer</Typography>
-                <Typography variant="body1">Footer</Typography>
-                <Typography variant="body1">Footer</Typography>
-            </BottomNavigation>
+           
           </div>
     );
 }
