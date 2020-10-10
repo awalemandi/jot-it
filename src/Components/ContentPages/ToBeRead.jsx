7330e87dee84e2a42de3f5be6c30fe1b2f59bb24
Joanna Cheok <jo@Joannas-MacBook-Pro.local>
2020-10-10 14:45:47 +1100

    imported simple list for TBR list

--- src/Components/ContentPages/ToBeRead.jsx
+++ src/Components/ContentPages/ToBeRead.jsx
@@ -1,10 +1,71 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Divider, List, ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText, Checkbox, IconButton } from '@material-ui/core';
+import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        margin: theme.spacing(0, 0, 0, 2),
+        padding: theme.spacing(0, 0, 0, 2),
+        minHeight: 400,
+    },
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
 
 const ToBeRead = () => {
+    const classes = useStyles();
+    const [checked, setChecked] = React.useState([0]);
+
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
     return (
-        <>
-            TBR Page
-        </>
+        <div className={classes.paper} overflow="visible">
+            <Typography component="h1" variant="h6">
+                    To Be Read
+                    <Divider />
+            </Typography>
+
+            <List className={classes.root}>
+            {[0, 1, 2, 3].map((value) => {
+                const labelId = `checkbox-list-label-${value}`;
+
+                return (
+                    <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
+                        <ListItemIcon>
+                            <Checkbox
+                                edge="start"
+                                checked={checked.indexOf(value) !== -1}
+                                tabIndex={-1}
+                                disableRipple
+                                inputProps={{ 'aria-labelledby': labelId }}
+                            />
+                        </ListItemIcon>
+                        <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                        <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="comments">
+                                <CancelRoundedIcon />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                );
+            })}
+        </List>
+        </div>
     )
 };
 
